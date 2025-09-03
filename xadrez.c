6fa6e2e17a33b8496d7fd6082e74a94b9e18cf1a
LUCASREIS23 <202501458289@alunos.estacio.br>
2025-09-02 23:51:26 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,32 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    // quantidade de casas por peca
+    int casasrainha = 8;
+    int casasbispo = 5;
+    int casastorre = 5;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // movimentaçao da torre (for)
+    printf("Movimento da Torre\n");
+    for ( int i = 1; i<=casastorre; i++) {
+            printf("Direita\n");
+    }
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    //movimentaçao bispo (while)
+        printf("Movimento da Bispo\n");
+        int i = 1;
+    while (i <= casasbispo) {
+        printf("Cima,Direita\n");
+        i++;
+    }
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    // movimentaçao rainha (do-while)
+    printf ("Movimento Rainha\n");
+    int j = 1;
+    do {
+        printf("esquerda\n");
+        j++;
+    }while (j <= casasrainha);
 
     return 0;
 }
